7de5cd956a949a2182d3931c7c68206ff49c1809
leedhfsd <leedhfsd@gmail.com>
2022-10-15 20:46:23 +0900

    done

--- models/article.js
+++ models/article.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose')
-const { marked } = require('marked')
-const slugify = require('slugify')
-const createDomPurify = require('dompurify')
-const { JSDOM } = require('jsdom')
-const dompurify = createDomPurify(new JSDOM().window)
+const mongoose = require('mongoose');
+const { marked } = require('marked');
+const slugify = require('slugify');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
 
 const articleSchema = new mongoose.Schema({
   title: {
@@ -30,18 +30,18 @@ const articleSchema = new mongoose.Schema({
     type: String,
     required: true
   }
-})
+});
 
 articleSchema.pre('validate', function(next) {
   if (this.title) {
-    this.slug = slugify(this.title, { lower: true, strict: true })
+    this.slug = slugify(this.title, { lower: true, strict: true });
   }
 
   if (this.markdown) {
-    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+    this.sanitizedHtml = dompurify.sanitize(marked.parse(this.markdown));
   }
 
-  next()
+  next();
 })
 
-module.exports = mongoose.model('Article', articleSchema)
+module.exports = mongoose.model('Article', articleSchema);
